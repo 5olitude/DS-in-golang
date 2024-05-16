6015c5c149898d86dd986a850b0701521ecaef0f
Joseph Hermis <43909665+5olitude@users.noreply.github.com>
2024-05-16 21:34:54 +0530

    Update linear_table_delete.go

--- linear_table_delete.go
+++ linear_table_delete.go
@@ -1,1 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{1, 3, 5, 9, 4}
+	length := len(arr)
+	temp := make([]int, length-1)
+	index := 0
+
+	for i := 0; i < length; i++ {
+		if i < index {
+			temp[i] = arr[i]
+
+		}
+		if i > index {
+			temp[i-1] = arr[i]
+
+		}
+
+	}
+	fmt.Println(temp)
+}
 
